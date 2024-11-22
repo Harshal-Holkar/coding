bf41775d45241e73db1e6b4090f847438ca3cd49
Harshal Holkar <93507639+Harshal-Holkar@users.noreply.github.com>
2024-11-22 09:06:15 +0530

    Create cpp bolierplatecode

---
+++ boilerPlateCode.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    
+}
+
+
+int32_t main(){
+    #ifndef ONLINE_JUDGE
+    freopen("i.txt", "r", stdin); freopen("o.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+    int t=1;
+    cin >> t;
+     for(int i = 1; i <= t; i++)
+         solve();
+    return 0;
+}
